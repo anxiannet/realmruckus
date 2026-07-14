efcbd687efda1cc1c395a00125fbac67f000e2d7
anxiannet <zhao.qihong@gmail.com>
2026-07-14 17:24:33 +0800

    导出通用卡表加载接口

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,12 +1,18 @@
 from .combat import resolve_attack, resolve_duel
+from .data import CardDefinition, DeckDefinition, combine_decks, load_deck, validate_deck
 from .models import Area, AttackResult, Unit
 from .state import GameState
 
 __all__ = [
     "Area",
     "AttackResult",
+    "CardDefinition",
+    "DeckDefinition",
     "GameState",
     "Unit",
+    "combine_decks",
+    "load_deck",
     "resolve_attack",
     "resolve_duel",
+    "validate_deck",
 ]
