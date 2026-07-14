9f816243eb6896b17d97bfaf485e9f1336cc1779
anxiannet <zhao.qihong@gmail.com>
2026-07-14 17:34:48 +0800

    导出行动回合与效果接口

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,17 +1,43 @@
+from .actions import (
+    ActionType,
+    AttackAction,
+    DeployAction,
+    PassAction,
+    is_legal_attack,
+    is_legal_deploy,
+    legal_attack_targets,
+    legal_deploy_destinations,
+)
 from .combat import resolve_attack, resolve_duel
 from .data import CardDefinition, DeckDefinition, combine_decks, load_deck, validate_deck
+from .effects import EffectContext, execute_effect, registered_effect_ids
 from .models import Area, AttackResult, Unit
 from .state import GameState
+from .turns import TurnPhase, TurnState, phase_order
 
 __all__ = [
+    "ActionType",
     "Area",
+    "AttackAction",
     "AttackResult",
     "CardDefinition",
     "DeckDefinition",
+    "DeployAction",
+    "EffectContext",
     "GameState",
+    "PassAction",
+    "TurnPhase",
+    "TurnState",
     "Unit",
     "combine_decks",
+    "execute_effect",
+    "is_legal_attack",
+    "is_legal_deploy",
+    "legal_attack_targets",
+    "legal_deploy_destinations",
     "load_deck",
+    "phase_order",
+    "registered_effect_ids",
     "resolve_attack",
     "resolve_duel",
     "validate_deck",
